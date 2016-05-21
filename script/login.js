cf8934fa05801a515e5c591e9227db0a302554a2
liuxuewen <457460089@qq.com>
2016-05-21 18:24:58 +0800

    login页面js

---
+++ script/login.js
@@ -0,0 +1,40 @@
+$(function(){
+	var $name = $('#login-phone'),
+		$pwd = $('#login-pwd'),
+		$error = $('.login-error');
+	var _envir = 'local';
+
+	$('.login-in input').on('focus', function(){
+		$error.html('');
+		$(this).val('');
+	})
+
+	$('.login-submit').on('click', function(){
+		if($.trim($name.val()) == ''){
+			$error.html('请输入邮箱／手机号码！');
+			return;
+		}
+		if($.trim($pwd.val()) == ''){
+			$error.html('请输入登录密码！');
+			return;
+		}
+
+		var params = {
+			url: config_ajax[_envir].login,
+			type: config_ajax[_envir].type,
+			data: {
+				username: $.trim($name.val()),
+				password: $.trim($pwd.val()),
+				grant_type: 'password',
+				client_id: 'hongtui',
+				client_secret: 'hongtui'
+			},
+			succFn: function(data){
+				if(data.errorCode == 0){
+					//成功
+				}
+			}
+		};
+		Util.requestAjaxFn(params);
+	});
+})
