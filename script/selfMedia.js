5c1ae97d0f37da001888f50e8daad503e7599595
liuxuewen <457460089@qq.com>
2016-05-22 10:11:52 +0800

    分页

--- script/selfMedia.js
+++ script/selfMedia.js
@@ -3,6 +3,7 @@ $(function(){
 		data: {
 			Page: 0,
 			PageSize: 10,
+			TotalPage: 0,
 			Category: ""
 		},
 		_envir: 'local'
@@ -34,14 +35,45 @@ $(function(){
 		var ajaxParams = {
 			url: config_ajax[config._envir].getMedia,
 			type: config_ajax[config._envir].type,
-			data: $.extend(dataParams, config.data),
+			data: $.extend(config.data, dataParams),
 			succFn: function(data){
+				data.TotalPage = Math.ceil(data.TotalCount/data.PageSize) - 1;
+				config.data.TotalPage = data.TotalPage;
 				$("#result-con").html(template('temp_list', data));
+				$("#page_flip").html(template('temp_flip', data))
 			}
 		};
 		Util.requestAjaxFn(ajaxParams);
 	};
 	getList();
+	//首页、上一页、下一页、末页
+	$("#page_flip").on('click', '.homepage', function(){
+		if($(this).hasClass('disabled')) return;
+
+		config.data.Page = 0;
+		getList();
+	}).on('click', '.previous', function(){
+		if($(this).hasClass('disabled')) return;
+		
+		var page = config.data.Page - 1;
+		if(page >= 0){
+			config.data.Page = page;
+		}
+		getList();
+	}).on('click', '.next', function(){
+		if($(this).hasClass('disabled')) return;
+		
+		var page = config.data.Page + 1;
+		if(page <= config.data.TotalPage){
+			config.data.Page = page;
+		}
+		getList();
+	}).on('click', '.lastpage', function(){
+		if($(this).hasClass('disabled')) return;
+
+		config.data.Page = config.data.TotalPage;
+		getList();
+	})
 
 	//绑定切换查询类型的点击事件
 	$(".find-items .item").on('click', function(){
