7ef3ed94fec0cf87ce0e6a6c4b4fc45a493da4db
liuliyuan <liuliyuan@souyidai.com>
2016-05-22 13:39:53 +0800

    冲突调整

--- script/connect.js
+++ script/connect.js
@@ -5,12 +5,9 @@ var config_ajax = {
 		'userInfo': './script/json/userInfo.json',
 		'getMedia': './script/json/selfMedia.json',
 		'operFav': './script/json/operFav.json',
-<<<<<<< HEAD
 		'cart':'./script/json/Cart.json',
-		'getfavo':'./script/json/selfMedia.json'
-=======
+		'getfavo':'./script/json/selfMedia.json',
 		"getCart": './script/json/getCart.json'
->>>>>>> 279b96d03826c77c55bfbeec636d16da07bd74ee
 	},
 	server: {
 		'type': 'POST',
